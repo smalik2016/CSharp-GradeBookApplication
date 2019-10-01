051c672eeeedb326718e0320cff496ecf44f7440
s.malik2016 <s.malik@live.co.uk>
2019-10-01 13:25:10 +0100

    update cw

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -47,7 +47,8 @@ namespace GradeBook.GradeBooks
         {
             if (Students.Count < 5)
             {
-                throw new InvalidOperationException("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade");
+                Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade");
+                return;
             }
 
             base.CalculateStatistics();
@@ -55,10 +56,8 @@ namespace GradeBook.GradeBooks
 
         public override void CalculateStudentStatistics(string name)
         {
-            if (Students.Count < 5)
-            {
-                throw new InvalidOperationException("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade");
-            }
+            Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade");
+            return;
 
             base.CalculateStudentStatistics(name);
         }
