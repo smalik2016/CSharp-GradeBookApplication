43fdd977049a92074632365453ad3673214bef43
s.malik2016 <s.malik@live.co.uk>
2019-10-01 13:21:31 +0100

    Override stattistics

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -42,5 +42,27 @@ namespace GradeBook.GradeBooks
                 return 'F';
             }            
         }
+
+        public override void CalculateStatistics()
+        {
+            if (Students.Count < 5)
+            {
+                throw new InvalidOperationException("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade");
+            }
+
+            base.CalculateStatistics();
+        }
+
+        public override void CalculateStudentStatistics(string name)
+        {
+            if (Students.Count < 5)
+            {
+                throw new InvalidOperationException("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade");
+            }
+
+            base.CalculateStudentStatistics(name);
+        }
+
+
     }
 }
